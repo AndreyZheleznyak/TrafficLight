e30e82271852ce6e01c7e484e768db3ca30bdbe3
AndreyZheleznyak <zheleznyak.dev@gmail.com>
2022-07-10 18:02:06 +0300

    Connect labels and button. Customize labels and buttons. Write traffic light logic.

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -1,19 +1,59 @@
-//
-//  ViewController.swift
-//  TrafficLight
-//
-//  Created by admin on 08.07.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+// MARK: - IB Outlets
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    private let lightOn: CGFloat = 1
+    private let lightOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startButton.layer.cornerRadius = 10
+        
+        redLight.alpha = lightOff
+        yellowLight.alpha = lightOff
+        greenLight.alpha = lightOff
     }
-
-
+    
+    override func viewWillLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = redLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+    }
+    
+// MARK: - IB Actions
+    @IBAction func startButtonPressed() {
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+        
+    switch currentLight {
+        case .red:
+            greenLight.alpha = lightOff
+            redLight.alpha = lightOn
+            currentLight = .yellow
+        case .yellow:
+            redLight.alpha = lightOff
+            yellowLight.alpha = lightOn
+            currentLight = .green
+        case .green:
+            greenLight.alpha = lightOn
+            yellowLight.alpha = lightOff
+            currentLight = .red
+        }
+    }
 }
 
+// MARK: - CurrentLight
+extension ViewController {
+    private enum CurrentLight {
+        case red, yellow, green
+    }
+}
